0093dd9521005ad354080ab1561830aff9f8b729
Asmaparkar <87011227+Asmaparkar@users.noreply.github.com>
2021-07-12 00:19:28 +0530

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+
+function getdata(){
+    var Name = document.getElementById('name');
+    var Value =  Name.value;
+    document.getElementById('p1').innerText=" Hello , Welcome " + Value + " ... ";
+}
